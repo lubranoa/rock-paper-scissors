c3cfec950dd00154fff5a30c1a6a674ac46d477a
lubranoa <lubranoa@oregonstate.edu>
2021-09-08 10:32:47 -0500

    Add 5 round game feature

--- janken.js
+++ janken.js
@@ -18,26 +18,65 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     
-    // Standardizes capitalization of player's move
-    let playerMove = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-    console.log(playerMove);
+    if ((playerSelection == 'Rock' && computerSelection == 'Paper') 
+    || (playerSelection == 'Paper' && computerSelection == 'Scissors') 
+    || (playerSelection == 'Scissors' && computerSelection == 'Rock')) {
+        return 0;
     
-    if ((playerMove == 'Rock' && computerSelection == 'Paper') 
-    || (playerMove == 'Paper' && computerSelection == 'Scissors') 
-    || (playerMove == 'Scissors' && computerSelection == 'Rock')) {
-        return `You Lose! ${computerSelection} beats ${playerMove}`;
-    
-    } else if ((playerMove == 'Rock' && computerSelection == 'Scissors') 
-    || (playerMove == 'Paper' && computerSelection == 'Rock') 
-    || (playerMove == 'Scissors' && computerSelection == 'Paper')) {
-        return `You Win! ${playerMove} beats ${computerSelection}`;
+    } else if ((playerSelection == 'Rock' && computerSelection == 'Scissors') 
+    || (playerSelection == 'Paper' && computerSelection == 'Rock') 
+    || (playerSelection == 'Scissors' && computerSelection == 'Paper')) {
+        return 1;
     
     } else {
-        return `You tied this round!`;
+        return -1;
     }
 }
 
-const playerMove = 'rock';
-const computerMove = computerPlay();
+function isValidMove(playerSelection) {
+    let validMoves = ['rock', 'paper', 'scissors']
+    if (validMoves.includes(playerSelection) == true) {
+        return playerSelection;
+    } else {
+        playerMove = prompt('Invalid move. Please try again.');
+        isValidMove(playerMove.toLowerCase());
+    }
+    return playerMove;
+}
 
-console.log(playRound(playerMove, computerMove));
+function game() {
+    let roundCounter = 0;
+    let playerWins = 0;
+    let computerWins = 0;
+    let ties = 0;
+    while (roundCounter < 5) {
+        let playerMove = prompt(`Round ${roundCounter+1} of 5. Please enter a move.`);
+        playerMove = isValidMove(playerMove.toLowerCase());
+        // Standardizes capitalization of player's move
+        const playerChoice = playerMove.charAt(0).toUpperCase() + playerMove.slice(1);
+        const computerMove = computerPlay();
+        roundResult = playRound(playerChoice, computerMove);
+        if (roundResult == 0) {
+            computerWins += 1;
+            roundCounter += 1;
+            alert(`Round ${roundCounter} lost! ${computerMove} beats ${playerMove}!`);
+        } else if (roundResult == 1) {
+            playerWins += 1;
+            roundCounter += 1;
+            alert(`Round ${roundCounter} won! ${playerMove} beats ${computerMove}!`);
+        } else {
+            ties += 1;
+            roundCounter += 1;
+            alert(`Round ${roundCounter} tied!`);
+        }
+    }
+    if (playerWins == computerWins) {
+        alert(`Game Over! You tied the computer with ${playerWins} wins each and ${ties} ${(ties == 1) ? 'tie' : 'ties'}!`);
+    } else if (playerWins < computerWins) {
+        alert(`Game Over! You lost to the computer ${computerWins} wins to ${playerWins} with ${ties} ${(ties == 1) ? 'tie' : 'ties'}!`);
+    } else {
+        alert(`YOU WON! You beat the computer ${playerWins} wins to ${computerWins} with ${ties} ${(ties == 1) ? 'tie' : 'ties'}!`);
+    }
+}
+
+game();
