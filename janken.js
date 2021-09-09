3cf7a471cb18dd941b18e363e4dfcfad8e106ce6
lubranoa <lubranoa@oregonstate.edu>
2021-09-09 06:44:10 -0500

    Add comments for readability

--- janken.js
+++ janken.js
@@ -18,16 +18,19 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     
-    if ((playerSelection == 'Rock' && computerSelection == 'Paper') 
-    || (playerSelection == 'Paper' && computerSelection == 'Scissors') 
-    || (playerSelection == 'Scissors' && computerSelection == 'Rock')) {
+    // If the computer won, returns 0
+    if ((playerSelection == 'Rock' && computerSelection == 'Paper') || 
+            (playerSelection == 'Paper' && computerSelection == 'Scissors') || 
+            (playerSelection == 'Scissors' && computerSelection == 'Rock')) {
         return 0;
     
-    } else if ((playerSelection == 'Rock' && computerSelection == 'Scissors') 
-    || (playerSelection == 'Paper' && computerSelection == 'Rock') 
-    || (playerSelection == 'Scissors' && computerSelection == 'Paper')) {
+    // Else, if the player won, return 1
+    } else if ((playerSelection == 'Rock' && computerSelection == 'Scissors') || 
+            (playerSelection == 'Paper' && computerSelection == 'Rock') || 
+            (playerSelection == 'Scissors' && computerSelection == 'Paper')) {
         return 1;
     
+    // Otherwise, it was a tie and returns -1
     } else {
         return -1;
     }
@@ -35,41 +38,61 @@ function playRound(playerSelection, computerSelection) {
 
 function isValidMove(playerSelection) {
     let validMoves = ['rock', 'paper', 'scissors']
+    
+    // If the player's selection is in the valid moveset array, returns the move
     if (validMoves.includes(playerSelection) == true) {
         return playerSelection;
+    // Otherwise, recursively calls this function until a valid input is entered
     } else {
         playerMove = prompt('Invalid move. Please try again.');
         isValidMove(playerMove.toLowerCase());
     }
+
     return playerMove;
 }
 
 function game() {
+    
+    // Init counters for the number of rounds, wins, and ties
     let roundCounter = 0;
     let playerWins = 0;
     let computerWins = 0;
     let ties = 0;
+    
+    // Iterates for 5 rounds
     while (roundCounter < 5) {
+        
+        // Prompts the player for a move and checks validity
         let playerMove = prompt(`Round ${roundCounter+1} of 5. Please enter a move.`);
         playerMove = isValidMove(playerMove.toLowerCase());
+        
         // Standardizes capitalization of player's move
         const playerChoice = playerMove.charAt(0).toUpperCase() + playerMove.slice(1);
+        // Generates the computer's move
         const computerMove = computerPlay();
+        
+        // Gets and holds this round's result
         roundResult = playRound(playerChoice, computerMove);
+        
+        // If the computer won the round, increments counters and alerts player of lost round
         if (roundResult == 0) {
             computerWins += 1;
             roundCounter += 1;
             alert(`Round ${roundCounter} lost! ${computerMove} beats ${playerMove}!`);
+        // Else, if the player won it, increments counters and alerts player of won round
         } else if (roundResult == 1) {
             playerWins += 1;
             roundCounter += 1;
             alert(`Round ${roundCounter} won! ${playerMove} beats ${computerMove}!`);
+        // Otherwise, increments counters and alerts player of tied round
         } else {
             ties += 1;
             roundCounter += 1;
             alert(`Round ${roundCounter} tied!`);
         }
     }
+    
+    // After 5 rounds have been played, alerts the player whether they tied, lost, or won the game
     if (playerWins == computerWins) {
         alert(`Game Over! You tied the computer with ${playerWins} wins each and ${ties} ${(ties == 1) ? 'tie' : 'ties'}!`);
     } else if (playerWins < computerWins) {
